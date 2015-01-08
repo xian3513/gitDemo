1f49d8adacddd9cc05912511cb7e1681ec6597d2
kt <kt@a2ndeiMac.local>
2015-01-08 15:21:14 +0800

    demo

---
+++ GitDemo/GitDemo/testDemo.h
@@ -0,0 +1,13 @@
+//
+//  testDemo.h
+//  GitDemo
+//
+//  Created by kt on 15/1/8.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testDemo : NSObject
+
+@end
---
+++ GitDemo/GitDemo/testDemo.m
@@ -0,0 +1,13 @@
+//
+//  testDemo.m
+//  GitDemo
+//
+//  Created by kt on 15/1/8.
+//  Copyright (c) 2015年 kt. All rights reserved.
+//
+
+#import "testDemo.h"
+
+@implementation testDemo
+
+@end
